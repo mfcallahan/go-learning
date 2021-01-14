4ab89eebd70df8bae0da9f77436035360a5c003f
Matt Callahan <matthew.callahan@outlook.com>
2021-01-14 13:16:56 -0700

    build helloWorld

--- HelloWorld/go.mod
+++ HelloWorld/go.mod
@@ -1,5 +1,10 @@
-module hello
+module helloWorld
 
 go 1.15
 
-require rsc.io/quote v1.5.2 // indirect
+require (
+	example.com/greetings v0.0.0-00010101000000-000000000000
+	rsc.io/quote v1.5.2
+)
+
+replace example.com/greetings => ../SampleModule
--- HelloWorld/helloWorld.go
+++ HelloWorld/helloWorld.go
@@ -1,14 +1,15 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+	"rsc.io/quote"
+	"example.com/greetings"
+)
 
 func main() {
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
-
-	message := "Hello, world! (From Matt)."
-	fmt.Println(message)
+	fmt.Println(greetings.CustomHello("Matt"))
 }
