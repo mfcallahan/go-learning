cab54500a8dc3aa511c42ee632b50f675e1b7766
Matt Callahan <matthew.callahan@outlook.com>
2021-01-14 08:24:33 -0700

    use rsc.io/quote package

---
+++ HelloWorld/go.mod
@@ -0,0 +1,5 @@
+module helloWorld
+
+go 1.15
+
+require rsc.io/quote v1.5.2
--- HelloWorld/helloWorld.go
+++ HelloWorld/helloWorld.go
@@ -1,8 +1,14 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
 
 func main() {
-	message := "Hello, world!"
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+
+	message := "Hello, world! (From Matt)."
 	fmt.Println(message)
 }
